beff2dc8e4b2a5991b5d98c98a662bdf4f9edc69
jcerto4 <165083406+jcerto4@users.noreply.github.com>
2025-02-07 21:28:40 -0500

    Testing Updating Repository

---
+++ src/screens/QuestionScreen.java
@@ -0,0 +1,18 @@
+package screens;
+
+import classes.Question;
+import gameobjects.Timer;
+import javafx.scene.layout.BorderPane;
+
+public class QuestionScreen extends BorderPane{
+	
+	private Timer timer;
+	private Question question;
+	
+	
+	public QuestionScreen() {
+		
+	}
+	
+	
+}
